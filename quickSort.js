146bbff6711f0e23d9854bcffa1d06957bdf468a
Jarod Bibb <jbibb8@gmail.com>
2018-08-21 21:47:16 -0700

    calls, apply and bind

---
+++ quickSort.js
@@ -0,0 +1,34 @@
+// function quickSort(arr, i , j, pivot){
+//     console.log("first: ", i);
+//     if(arr.length < 2){
+//         return arr;
+//     }
+//     if(!i && !j){
+//         i = 0;
+//         j = arr.length -1 
+//     }
+//     if
+    //  var i = 0;
+    //  var j = arr.length -1
+    // var pivot = Math.floor(arr.length / 2)
+    // var counter = 0;
+//     if(i != j){
+//         console.log('inside')
+//         while(arr[i] < arr[pivot]){
+//             console.log('double inside')
+//             i++;
+//         }
+//         while(arr[j] > arr[pivot]){
+//             j--
+//         }
+//         if(arr[i] > arr[pivot] && arr[j] < arr[pivot]){
+//             var temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+
+//         }
+//     }
+//     console.log(arr)
+//     return arr
+// }
+// }
