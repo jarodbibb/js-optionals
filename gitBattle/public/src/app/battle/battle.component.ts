1c710608ef800fe6c473c9f9b889e6f319b5cf82
Jarod Bibb <jbibb8@gmail.com>
2018-10-09 00:25:06 -0700

    added feature to disable buttons

--- gitBattle/public/src/app/battle/battle.component.ts
+++ gitBattle/public/src/app/battle/battle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpService } from '../http.service';
+import {HttpService } from './../http.service';
 import { Router, ActivatedRoute, Params, Data } from '@angular/router';
+
 
 @Component({
   selector: 'app-battle',
@@ -8,17 +9,19 @@ import { Router, ActivatedRoute, Params, Data } from '@angular/router';
   styleUrls: ['./battle.component.css']
 })
 export class BattleComponent implements OnInit {
+
 swtich: boolean = false;
 switch2: boolean = false;
 button1: boolean = true;
 button2: boolean = true;
-guser1: string
+guser1: any;
 guser2: any;
 
 
-  constructor(private _httpService: HttpService, private _router: Router) { }
+  constructor(private _httpService: HttpService, private _router: Router ) { }
   
   ngOnInit() {
+    this.guser1 ={ name: ""}
     this.guser2 = {name: ""}
   }
   ready2(){
