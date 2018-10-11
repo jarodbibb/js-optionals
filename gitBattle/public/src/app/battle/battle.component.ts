01eb617978aeba0d4a067c901c1cc9c812053e25
Jarod Bibb <jbibb8@gmail.com>
2018-10-10 20:55:27 -0700

    battle function works

--- gitBattle/public/src/app/battle/battle.component.ts
+++ gitBattle/public/src/app/battle/battle.component.ts
@@ -10,13 +10,19 @@ import { Router, ActivatedRoute, Params, Data } from '@angular/router';
   styleUrls: ['./battle.component.css']
 })
 export class BattleComponent implements OnInit {
-
-swtich: boolean = false;
-switch2: boolean = false;
-button1: boolean = true;
-button2: boolean = true;
+  battlin: boolean = false;
+switch: boolean = false
+switch1: boolean = true;
+switch2: boolean =true;
 guser1: any;
 guser2: any;
+user1: any;
+user2: any;
+rankings: [any] = [""]
+ranking: {};
+sortable: [any]  =[{}]
+first: {any}
+second: {any}
 
 
   constructor(private _httpService: HttpService, private _router: Router ) { }
@@ -34,10 +40,58 @@ guser2: any;
   //   })
   // }
 
-  retrieveGithubUser(){
-    let obs = this._httpService.retrieveGithubUser(this.guser2.name);
+  retrieveGithubUser(data, formNum){
+    let obs = this._httpService.retrieveGithubUser(data.name);
     obs.subscribe(response => {
-      console.log("Data: " + JSON.stringify(response));
+      if(formNum === 1){
+        this.switch1 = false;
+        var score = response['followers'] + response['public_repos'] * 12;
+      
+        this.rankings.push({name: response['name'], score: score})
+        // console.log('rankings', this.rankings[0])
+      } else if( formNum === 2){
+        this.switch2 = false;
+        var score = response['followers'] + response['public_repos'] * 12;
+      
+        this.rankings.push({name: response['name'], score: score})
+      }if(this.switch1 === this.switch2){
+          this.switch = true;
+      }
     })
+  }
+        // if(this.rankings[0].score < score){
+        //   this.rankings.splice(0,0, score)
+        // }
+        // for(let i =1; i < this.rankings.length; ++){
+        //   if(this.rankings[i].score < score ){
+        //     this.rankings.splice(i-1, 0, score)
+        //   }
+        // }
+      
+        // this.rankings.push(response),
+        // console.log('testing to see rankings', this.rankings)
+        
+
+
+  startB(){
+    
+    this.battlin = true;
+
+    console.log("sliced", this.rankings)
+    for (var git in this.rankings){
+      this.sortable.push([git, this.rankings[git]])
+    }
+    this.sortable.sort((a, b) => {
+      return b[1] - a[1]
+
+    })
+    console.log('sorting ', this.sortable[2])
+    this.first = this.sortable[2][1]
+    this.second = this.sortable[3][1]
+
+    
+
+
+    // console.log('sortin something?', this.rankings, this.sortable)
   }
 }
